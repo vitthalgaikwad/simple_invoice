81e23a9ebb87e883dae04ae914e593690bbc23e7
vitthal.gaikwad <gaikwadvitthal04@gmail.com>
2017-08-07 07:14:15 -0700

    Add pg gem and configure

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -7,7 +7,7 @@ class Payment < ActiveRecord::Base
 
 	def validate_amount
 		errors.add(:amount, "can't be less than or equal to zero") if (amount.to_f <= 0.0)
-		total_payment = Payment.where(invoice_id: invoice_id).group('invoice_id').select('invoice_id, sum(amount) as total').first
+		total_payment = Payment.where(invoice_id: invoice_id).group('invoice_id,id').select('invoice_id, sum(amount) as total').first
 		errors.add(:base, "Payment amount should not exceed to it's invoice amount. Please check existing payments.") if (total_payment.try(:total).to_f + amount.to_f > invoice.amount.to_f)
 	end
 end
